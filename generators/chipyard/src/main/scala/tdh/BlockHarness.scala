0bb9b5fc1e191d1e4d3460717252f3aea8153c2b
Tim H <Heaton15@gmail.com>
2025-06-17 14:07:51 -0400

    Add a BlockHarness so that we can build custom hardware for testing with the Config system!

--- generators/chipyard/src/main/scala/tdh/BlockHarness.scala
+++ generators/chipyard/src/main/scala/tdh/BlockHarness.scala
@@ -1,10 +1,41 @@
 package chipyard.tdh
 
-import org.chipsalliance.cde.config.Field
+import chisel3._
+import chisel3.experimental.BaseModule
+import chisel3.reflect.DataMirror
+
+import org.chipsalliance.cde.config.{Field, Parameters}
 
 case object TopModuleKey extends Field[Option[() => BaseModule]](None)
-case object LazyTopModuleKey extends Field[Option[() => BaseModule]](None)
 
 class BlockHarness(implicit val p: Parameters) extends Module {
+
+  // Instantiate the custom block we are designing
+  // If more types are added, we can pattern match on the keys instead
+  val dut = Module(
+    p(TopModuleKey).getOrElse(throw new Exception(s"Unable to instantiate module referenced by TopModuleKey"))()
+  )
+
+  // This has to go to the TestDriver if that still exists?
+  val success = IO(Output(Bool()))
+  success :<= false.B
+
+  val io = DataMirror
+    .modulePorts(dut)
+    .filter { case (name, _) => name != "clock" && name != "reset" }
+    .foreach { case (name, port) =>
+      val io = IO(chiselTypeOf(port))
+      io.suggestName(name)
+      DataMirror.directionOf(io) match {
+        case ActualDirection.Input                 => port :<= io
+        case ActualDirection.Output                => io   :<= port
+        case ActualDirection.Bidirectional.Default => io   :<= port
+        case ActualDirection.Bidirectional.Flipped => port :<= io
+        case ActualDirection.Empty                 => println("Skipping ActualDirection.Empty in BlockHarness")
+        case ActualDirection.Unspecified           => println("Skipping ActualDirection.Unspecified in BlockHarness")
+        case _ => throw new Exception("Unrecognized port direction encountered for Datamirror.directionOf(...)")
+      }
+
+    }
 
 }
---
+++ generators/chipyard/src/main/scala/tdh/configs/Adder.scala
@@ -0,0 +1,18 @@
+package chipyard.tdh.configs
+
+import chisel3._
+import chipyard.tdh.TopModuleKey
+import org.chipsalliance.cde.config.Config
+
+
+// This is just a test for the custom BlockHarness that is build
+class Adder(dataWidth: Int) extends Module {
+  val a = IO(Input(UInt(dataWidth.W)))
+  val b = IO(Input(UInt(dataWidth.W)))
+  val z = IO(Output(UInt((dataWidth + 1).W)))
+  z :<= a +& b
+}
+
+class AdderConfig extends Config((_, _, _) => { case TopModuleKey =>
+      Some(() => new Adder(8))
+    })
